a2f50671ec79c1cbf79e76a662db48afd89e4859
onidemon37 <41202410+onidemon37@users.noreply.github.com>
2022-06-28 00:04:22 +0100

    pre-commit, setup.py, requirements and initial project layout

--- setup.py
+++ setup.py
@@ -21,9 +21,7 @@ REQUIRES_PYTHON = '>=3.6.0'
 VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
-REQUIRED = [
-    # 'requests', 'maya', 'records',
-]
+REQUIRED = ["psycopg2-binary", "SQLAlchemy"]
 
 # What packages are optional?
 EXTRAS = {
