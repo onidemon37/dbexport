5a08f768524aff44f3261aba2b5f8cdc33e4a8c4
onidemon37 <41202410+onidemon37@users.noreply.github.com>
2022-06-28 13:45:02 +0100

    database connection

--- dbexport/config.py
+++ dbexport/config.py
@@ -1,11 +1,17 @@
 import os
 from functools import lru_cache
+
 from sqlalchemy import create_engine
 
 
+@lru_cache(maxsize=32)
 def engine(db_url=None):
-    pass
+    db_url = db_url or os.getenv("DB_URL")
+    if not db_url:
+        raise ValueError("database URL is required")
+    print(f"Returning an engine for {db_url}")
+    return create_engine(db_url)
 
 
 def get_connection(db_url=None):
-    pass
+    return engine(db_url).connect()
