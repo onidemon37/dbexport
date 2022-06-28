7d8e4f9f4c1ba62e908e9c3fc0bd160cb01f2866
onidemon37 <41202410+onidemon37@users.noreply.github.com>
2022-06-28 13:57:38 +0100

    session class

--- dbexport/config.py
+++ dbexport/config.py
@@ -2,6 +2,7 @@ import os
 from functools import lru_cache
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 @lru_cache(maxsize=32)
@@ -15,3 +16,15 @@ def engine(db_url=None):
 
 def get_connection(db_url=None):
     return engine(db_url).connect()
+
+
+@lru_cache(maxsize=32)
+def session_class(db_url=None):
+    return sessionmaker(bind=engine(db_url))
+
+
+try:
+    Session = session_class()
+except:
+    print(f"Failed to create session class")
+    pass
